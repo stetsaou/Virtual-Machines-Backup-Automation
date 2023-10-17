b26ff2e7fc5d01371deb64361a2e1fd09b14157f
Stylianos Tsaousidis <stylianos.tsaousidis@accenture.com>
2023-10-17 12:11:08 +0300

    Updated

--- Powershell/BlueprintAssign.ps1
+++ Powershell/BlueprintAssign.ps1
@@ -9,25 +9,25 @@ az extension add --name blueprint
 
 if ($businessCriticality -eq "Low") {
     $blueprintName = "PPC-Low-Criticality"
-    $rg = @{ResourceGroup=@{name='backup-rg';location='westeurope'}}
+    $rg = @{ResourceGroup=@{name='backup-rg';location='LOCATION'}}
     $params = @{LowCritPolicy_vaultName="rsv-Backup"; LowCritPolicy_skuTier="Standard"; LowCritPolicy_policyName="BackupPolicy"; rsv_vaultName="rsv-Backup"; rsv_vaultStorageType="GeoRedundant"; rsv_location="LOCATION"; rsv_enableCRR= $true}
     $blueprintObject =  Get-AzBlueprint -ManagementGroupId 'MG' -Name $blueprintName
     New-AzBlueprintAssignment -Name "myAssignment" -Blueprint $blueprintObject -SubscriptionId $subscriptionid -Location "LOCATION" -ResourceGroupParameter $rg -Parameter $params
 } elseif ($businessCriticality -eq "Medium") {
     $blueprintName="PPC-Medium-Criticality"
-    $rg = @{ResourceGroup=@{name='backup-rg';location='westeurope'}}
+    $rg = @{ResourceGroup=@{name='backup-rg';location='LOCATION'}}
     $params = @{MedCritPolicy_vaultName="rsv-Backup"; MedCritPolicy_skuTier="Standard"; MedCritPolicy_policyName="BackupPolicy"; rsv_vaultName="rsv-Backup"; rsv_vaultStorageType="GeoRedundant"; rsv_location="LOCATION"; rsv_enableCRR= $true}
     $blueprintObject =  Get-AzBlueprint -ManagementGroupId 'MG' -Name $blueprintName
     New-AzBlueprintAssignment -Name "myAssignment" -Blueprint $blueprintObject -SubscriptionId $subscriptionid -Location "LOCATION" -ResourceGroupParameter $rg -Parameter $params
 } elseif ($businessCriticality -eq "High") {
     $blueprintName="PPC-High-Criticality"
-    $rg = @{ResourceGroup=@{name='backup-rg';location='westeurope'}}
+    $rg = @{ResourceGroup=@{name='backup-rg';location='LOCATION'}}
     $params = @{HighCritPolicy_vaultName="rsv-Backup"; HighCritPolicy_skuTier="Standard"; HighCritPolicy_policyName="BackupPolicy"; rsv_vaultName="rsv-Backup"; rsv_vaultStorageType="GeoRedundant"; rsv_location="LOCATION"; rsv_enableCRR= $true}
     $blueprintObject =  Get-AzBlueprint -ManagementGroupId 'MG' -Name $blueprintName
     New-AzBlueprintAssignment -Name "myAssignment" -Blueprint $blueprintObject -SubscriptionId $subscriptionid -Location "LOCATION" -ResourceGroupParameter $rg -Parameter $params
 } elseif ($businessCriticality -eq "Critical") {
     $blueprintName="PPC-Critical-Criticality"
-    $rg = @{ResourceGroup=@{name='backup-rg';location='westeurope'}}
+    $rg = @{ResourceGroup=@{name='backup-rg';location='LOCATION'}}
     $params = @{CritPolicy_vaultName="rsv-Backup"; CritPolicy_skuTier="Standard"; CritPolicy_policyName="BackupPolicy"; rsv_vaultName="rsv-Backup"; rsv_vaultStorageType="GeoRedundant"; rsv_location="LOCATION"; rsv_enableCRR= $true}
     $blueprintObject =  Get-AzBlueprint -ManagementGroupId 'MG' -Name $blueprintName
     New-AzBlueprintAssignment -Name "myAssignment" -Blueprint $blueprintObject -SubscriptionId $subscriptionid -Location "LOCATION" -ResourceGroupParameter $rg -Parameter $params
